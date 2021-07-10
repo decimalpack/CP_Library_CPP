24d25055ab7e61d1cf03ab2f3d5a00a9d3850f8f
decimalpack <decimalpack@users.noreply.github.com>
2021-07-10 15:31:18 +0530

    Update tests for fenwick_PQRU

--- tests/fenwick_PQRU.cpp
+++ tests/fenwick_PQRU.cpp
@@ -9,18 +9,18 @@ TEST_CASE("Entire array")
 	const int n = 1000;
 	vector<int> arr(n);
 	iota(arr.begin(), arr.end(), 1);
-	Fenwick_Tree_PQRU<int> ft(arr);
+	FenwickTree_PQRU<int> ft(arr);
 
 	SECTION("From -100 to 100")
 	{
 		int total = 0;
 		for (int delta = -100; delta <= 100; delta++)
 		{
-			ft.range_update(1, n, delta);
+			ft.range_update(0, n - 1, delta);
 			total += delta;
 			for (int i = 0; i < n; i++)
 			{
-				REQUIRE(ft.point_query(i + 1) == arr[i] + total);
+				REQUIRE(ft.point_query(i) == arr[i] + total);
 			}
 		}
 	}
@@ -34,15 +34,15 @@ TEST_CASE("Benchmark", "[.benchmark]")
 		BENCHMARK("Array size: " + to_string(n))
 		{
 			CAPTURE(arr);
-			Fenwick_Tree_PQRU<int> ft(arr);
+			FenwickTree_PQRU<int> ft(arr);
 			int total = 0;
 			for (int delta = -5; delta <= 5; delta++)
 			{
-				ft.range_update(1, n, delta);
+				ft.range_update(0, n - 1, delta);
 				total += delta;
 				for (int i = 0; i < n; i++)
 				{
-					REQUIRE(ft.point_query(i + 1) == arr[i] + total);
+					REQUIRE(ft.point_query(i) == arr[i] + total);
 				}
 			}
 		};
@@ -55,11 +55,11 @@ TEST_CASE("Subarray")
 	const int n = 25;
 	vector<int> arr(n);
 	iota(arr.begin(), arr.end(), 1);
-	Fenwick_Tree_PQRU<int> ft(arr);
+	FenwickTree_PQRU<int> ft(arr);
 
-	for (int l = 1; l <= n; l++)
+	for (int l = 0; l < n; l++)
 	{
-		for (int r = l; r <= n; r++)
+		for (int r = l; r < n; r++)
 		{
 			stringstream test_name;
 			test_name << "Subarray: [" << l << ", " << r << "]";
@@ -72,7 +72,7 @@ TEST_CASE("Subarray")
 					total += delta;
 					for (int i = l; i <= r; i++)
 					{
-						REQUIRE(ft.point_query(i) == arr[i - 1] + total);
+						REQUIRE(ft.point_query(i) == arr[i] + total);
 					}
 				}
 			}
